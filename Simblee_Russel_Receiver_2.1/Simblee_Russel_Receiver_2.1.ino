7fb2ac0030226bf53cfef4e231f1b64e2d01e1c2
Scott Mitchell <scott@openobject.org>
2016-06-28 10:54:50 +1000

    added master controller
    this may break the puck communication.
    the puck won’t work unless it has been turned on by the master.

--- Simblee_Russel_Receiver_2.1/Simblee_Russel_Receiver_2.1.ino
+++ Simblee_Russel_Receiver_2.1/Simblee_Russel_Receiver_2.1.ino
@@ -29,7 +29,6 @@
 #include "SimbleeCOM.h"
 
 // the controller
-// this number can be big (<4,294,967,295) so you need to use an unsigned long
 unsigned long controlESN = 0xF8F31897;
 // the master
 unsigned long masterESN = 0x281a72e9;
@@ -57,9 +56,11 @@ const int VoltPin = 6;
 const int thresholdLow = 170;
 const int thresholdMed = 220;
 
+int motorValue = 0;
 unsigned long motorStart = 0;
 unsigned long timeout = 5000;
-int val = 0;
+// set by master controller
+boolean puckActive = false;
 
 
 void setup() {
@@ -88,8 +89,6 @@ void setup() {
 
 
 void loop() {
-  int motorValue = 0;
-
   // check the battery voltage
   int voltValue = analogRead(VoltPin);
   int mapValue = map(voltValue, 0, 1023, 0, 330);
@@ -113,50 +112,41 @@ void loop() {
   if (DEBUG) Serial.print("    ");
   if (DEBUG) Serial.print(mapValue);
 
-  if (rx_flag == true) {
-    // get payload data
-    motorValue = received.val;
-    motorStart = millis();
-
-    // for debuggning
-    if (DEBUG) Serial.print("   motorValue ");
-    if (DEBUG) Serial.print(received.val);
-
-    // reset payload flag
-    rx_flag = false;
-  }
-
+  // check for active puck
+  if (!puckActive) motorValue = 0;
   // check for motor timeout
   if (millis() - motorStart > timeout) motorValue = 0;
+  // set motor speed
   analogWrite(motorPin, motorValue);
 
   if (DEBUG) Serial.println("");
 
-  // Ultra Low Power Delay in milliseconds - this may not work
-  // also try Simblee_ULPDelay(INFINITE)
+  // Ultra Low Power Delay in milliseconds - this does not work
+  // Simblee_ULPDelay(INFINITE) - no bluetooth interupt
   // Simblee_ULPDelay(100);
   delay(100);
 }
 
 void SimbleeCOM_onReceive(unsigned int esn, const char *payload, int len, int rssi) {
+  // copy data into payload
+  memcpy(&received, payload, len);
+
   // check message source
-  if (esn == controlESN) {
+  if (esn == masterESN) {
+    // if the paylod is this Simblee's ESN then make this puck active
+    if (received.val == SimbleeCOM.getESN()) puckActive = true;
+    else puckActive = false;
+  } else if (puckActive) {
     // if the message came from the paired controller
-    // set indicator LED
-    digitalWrite(ledPinBlue, HIGH);
-
-    // copy data into payload
-    memcpy(&received, payload, len);
-
-    // set recive flag
-    rx_flag = true;
-
-    delay(100);
-    digitalWrite(ledPinBlue, LOW);
-
-  } else if (esn == masterESN) {
-    // if the message came from the master
-    // set the module to ?
+    if (esn == controlESN) {
+      // set indicator LED
+      digitalWrite(ledPinBlue, HIGH);
+      // set motor value
+      motorValue = received.val;
+      motorStart = millis();
+      delay(100);
+      digitalWrite(ledPinBlue, LOW);
+    }
   }
 
   if (DEBUG) Serial.print(" rssi: ");
